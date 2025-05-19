fdf35dff45e189a2d9c55de0b00a79891a862055
CarlosEduardo113 <ce219560@gmail.com>
2025-05-19 08:27:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,41 +1,23 @@
-﻿Console.Write("Digite o lado a, bora: ");
-        double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n\t\t\tBem-vindo(a) ao Banco de Empréstimos!");
 
-        Console.Write("Digite o lado b, bora: ");
-        double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\nPor favor, digite o valor que deseja.");
+double vEmprestimo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\nPor favor, digite a quantidade de parcelas desejada.");
+int qParcelas = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nPor favor, digite a sua renda mensal.");
+double vRenda = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Digite o lado c, bora: ");
-        double c = Convert.ToDouble(Console.ReadLine());
+double vParcela = vEmprestimo / qParcelas;
 
-        if (a <= 0 || b <= 0 || c <= 0)
-        {
-            Console.WriteLine("Todos os lados devem ser maiores que zero!");
-            return;
-        }
+double limite = vRenda * 0.3;
 
-        if (a < b + c && a > Math.Abs(b - c) &&
-            b < a + c && b > Math.Abs(a - c) &&
-            c < a + b && c > Math.Abs(a - b))
-        {
-            if (a == b && b == c)
-            {
-                Console.WriteLine("Triângulo Equilátero");
-            }
-            else if (a != b && b != c && a != c)
-            {
-                Console.WriteLine("Triângulo Escaleno");
-            }
-            else
-            {
-                Console.WriteLine("Triângulo Isósceles");
-            }
+Console.WriteLine("\n\nAnalisando... Aguarde...");
 
-            double s = (a + b + c) / 2;
-            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
-
-            Console.WriteLine($"Área do triângulo: {area:F2}");
-        }
-        else
-        {
-            Console.WriteLine("Os valores informados não formam um triângulo.");
-        }
+if (vParcela <= limite)
+{
+    Console.WriteLine("\nEmpréstimo Autorizado!");
+}
+else
+{
+    Console.WriteLine("\nEmpréstimo Negado.");
+}
